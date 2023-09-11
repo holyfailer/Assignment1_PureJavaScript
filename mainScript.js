b87d7699a88a7006361662ee2994450f5cb672fe
Sigurd Andersen <sigurdschelde@gmail.com>
2023-09-11 15:14:22 +0200

    finished final errors and comments

--- mainScript.js
+++ mainScript.js
@@ -7,16 +7,24 @@ const laptopPrice = document.getElementById('laptopPrice');
 const buyNowBtn = document.getElementById('buyNowBtn');
 const purchaseStatus = document.getElementById('purchaseStatus');
 
+const getLoanBtn = document.getElementById('getLoanBtn')
+const workBankBtn = document.getElementById('workBankBtn')
+const workBtn = document.getElementById('workBtn')
+const repayLoanBtn = document.getElementById('repayLoanBtn')
+
+
+//initialises balance, loan and pay. Balance starts at 200 kr
 let currentBalance = 200;
 
 let loanAmount = 0;
 
 let payAmount = 0;
 
-import { getLoan } from "./myBank.js";
-import { work } from "./myBank.js";
 
-
+getLoanBtn.addEventListener('click', getLoan)
+workBankBtn.addEventListener('click', transferPayToBank)
+workBtn.addEventListener('click', work)
+repayLoanBtn.addEventListener('click', payLoan)
 
 //Updates the visuals for the balance field. Is used whenever the balance is changed. 
 function updateBalance(){
@@ -44,12 +52,30 @@ function updateLoan(){
     elem.innerHTML = `Current Balance: ${Formatter.format(payAmount)}`;
 }
 
+//Function for getting a loan. IT prompts the user enter how much they want to loan. 
+//They are only allowed to if they doesn't already have a loan or if the loan is bigger than size the avaiable balance
+function getLoan(){
+    const loadInput= prompt("Enter loan amount:");
+    const amount  = parseInt(loadInput);
 
+    if(amount > 0 && amount <= currentBalance*2 && loanAmount == 0 ){
+        currentBalance += amount;
+        loanAmount +=amount;
+        updateBalance();
+        updateLoan();
+        document.querySelector('.repayLoanBtn').style.display = 'block';
+    } 
+}
+
+//work function. Adds 100 kr to the payAmount
+function work(){
+    payAmount+=100;
+    updatePay();
+}
 
 
 //transfers money to the bank from work. If there is no loan, transfer all of it, otherwise transfer 10% to the bankloan. 
 function transferPayToBank(){
-    console.log("hey" + loanAmount)
     if(loanAmount <= 0){
         currentBalance += payAmount;
     }else{
@@ -64,7 +90,7 @@ function transferPayToBank(){
     if(loanAmount < 0){
       let temp = Math.abs(loanAmount)   
       currentBalance += temp  
-      
+      loanAmount=0;
     }
     
     payAmount=0;
@@ -73,13 +99,16 @@ function transferPayToBank(){
     updateBalance()
 
 }
-
+//Pays the loan in full. Like above, any "negative" debt is returned to the balance
 function payLoan(){
+    console.log(loanAmount)
+    console.log(payAmount)
     loanAmount -= payAmount
     
     if(loanAmount < 0){
         let temp = Math.abs(loanAmount)   
         currentBalance += temp  
+        loanAmount=0;
         
       }
     payAmount=0
@@ -89,18 +118,21 @@ function payLoan(){
     updateBalance()
 }
 
+//Function that fetches all the laptops and info from the url.
+
 fetch('https://hickory-quilled-actress.glitch.me/computers')
     .then(response => response.json())
     .then(data => {
-
-
         data.forEach(laptop => {
             const option = document.createElement('option');
             option.value = laptop.id;
             option.textContent = laptop.title;
             laptopSelect.appendChild(option);
         });
+        //formatter for price to danish style.
+        const Formatter = new Intl.NumberFormat('da-DK',{style: 'currency', currency:'Dkk'} )
 
+        //eventlistener for the dropdown menu for selecting laptops. Displays the info 
         laptopSelect.addEventListener('change', () => {
             const selectedLaptopId = parseInt(laptopSelect.value);
             const selectedLaptop = data.find(laptop => laptop.id === selectedLaptopId);
@@ -110,19 +142,32 @@ fetch('https://hickory-quilled-actress.glitch.me/computers')
             laptopImage.src = `https://hickory-quilled-actress.glitch.me/${selectedLaptop.image}`;
             laptopName.textContent = selectedLaptop.title;
             laptopDescription.textContent = selectedLaptop.description;
-            laptopPrice.textContent = `Price: $${selectedLaptop.price}`;
+            laptopPrice.textContent = `Price: ${Formatter.format(selectedLaptop.price)}`;
+            
+            
+        });
 
-            buyNowBtn.addEventListener('click', () => {
-                 bankBalance = currentBalance; 
-                if (selectedLaptop.price <= bankBalance) {
-                    bankBalance -= selectedLaptop.price;
-                    purchaseStatus.textContent = `Congratulations! You are now the owner of the ${selectedLaptop.title}. Remaining balance: $${bankBalance}`;
-                } else {
-                    purchaseStatus.textContent = `Sorry, you cannot afford the ${selectedLaptop.title}. Your balance is insufficient.`;
-                }
-            });
+        // Trigger the change event after populating the dropdown menu so the content of the dropdown menu is populated the first time you start the page. 
+        const event = new Event('change');
+        laptopSelect.dispatchEvent(event);
+
+        //Event listener for buy laptop button with alert for giving a message if you can afford it or not. 
+        buyNowBtn.addEventListener('click', () => {
+            const selectedLaptopId = parseInt(laptopSelect.value);
+            const selectedLaptop = data.find(laptop => laptop.id === selectedLaptopId);
+
+            if (selectedLaptop.price <= currentBalance) {
+                alert(`Congratulations! You are now the owner of the ${selectedLaptop.title}. Remaining balance: $${currentBalance}`);
+                currentBalance -= selectedLaptop.price;
+                updateBalance();
+            } else {
+                alert(`Sorry, you cannot afford the ${selectedLaptop.title}. Your balance is insufficient.`);
+            }
         });
     });
+
+
+
 
 
 
@@ -132,4 +177,6 @@ fetch('https://hickory-quilled-actress.glitch.me/computers')
 updateBalance();
 updateLoan();
 updatePay();
+
+
 
